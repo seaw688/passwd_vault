6b1e1e50c18d70e920076297a06454174d73bc44
honcharov vladislav <snork98@gmail.com>
2020-05-23 01:51:26 +0300

    Added example of args parsing

--- automated_promt.py
+++ automated_promt.py
@@ -1,3 +1,9 @@
 #############################################################################################################################
-#Block with automated command promt (when neeaded function pre-set) #########################################################
+# Block with automated command promt (when neeaded function pre-set) #########################################################
 #############################################################################################################################
+
+
+# Function which called from main and get params from command line
+
+def python_chad_function_go_brrbrbr(magic_word: str):
+    print(f'Python goooooooo brrr brrr mazafaka and say "{magic_word}" nigga!!!')
--- main.py
+++ main.py
@@ -4,19 +4,33 @@ import os
 import global_variables
 import static
 from encryption import generate_keys_pare
+
 #############################################################################################################################
 
-#Check keys exist
-if (os.path.isfile(global_variables.private_key_file) 
-    and os.path.isfile(global_variables.public_key_file)):
+# Check keys exist
+if (os.path.isfile(global_variables.private_key_file)
+        and os.path.isfile(global_variables.public_key_file)):
     pass
-elif (os.path.isfile(global_variables.encrypted_vault_file) 
-    and (not os.path.isfile(global_variables.private_key_file 
-    or not os.path.isfile(global_variables.private_key_file)))): 
+elif (os.path.isfile(global_variables.encrypted_vault_file)
+      and (not os.path.isfile(global_variables.private_key_file
+                              or not os.path.isfile(global_variables.private_key_file)))):
     print('Error! Vault exist but public or private key not exist!')
-else: 
-    generate_keys_pare()  
+else:
+    generate_keys_pare()
 
-#Check that command-line param exist
+# Check that command-line param exist
 # If exist - go to the "automated.py" (there are we call functions from vault.py with arguments that we enter in command promt)
 # If not - "go to the interactive.py" (there are we call functios from vault.py after programm ask as neeaded functions, desired variable values, etc)
+
+import argparse
+
+parser = argparse.ArgumentParser(description='I am so fucking hot and horny hacker bitch')
+parser.add_argument('--ehai-nahui', action="store", dest="popizdovali", type=str)
+args = parser.parse_args()
+
+if args.popizdovali:
+    from automated_promt import python_chad_function_go_brrbrbr
+    python_chad_function_go_brrbrbr(args.popizdovali)
+else:
+    from interactive import virgin_function
+    virgin_function()
